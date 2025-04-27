683b5d2b88e4f1f9a6902bb0402266e7b349c24b
zizicici <1940699+zizicici@users.noreply.github.com>
2025-04-27 21:45:53 +0800

    [misc] Initial Project

---
+++ Tag Day/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tag Day
+//
+//  Created by Ci Zi on 27/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
