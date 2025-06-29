80d5e251aa05eda42a756a8e95e35319efec21ae
zizicici <1940699+zizicici@users.noreply.github.com>
2025-06-29 14:14:35 +0800

    [feat] Initial Tag Widget

---
+++ Tag Widget/TagWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Ci Zi on 2025/6/29.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TagWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TagWidget()
+    }
+}
