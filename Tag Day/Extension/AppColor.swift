fdd282661be763bf291339e62e9ee7d949d220ed
zizicici <1940699+zizicici@users.noreply.github.com>
2025-07-05 15:59:03 +0800

    [feat] Sufficient Contrast

--- Tag Day/Extension/AppColor.swift
+++ Tag Day/Extension/AppColor.swift
@@ -10,13 +10,8 @@ import UIKit
 struct AppColor {
     static let main = UIColor.main
     static let text = UIColor.text
-    static let tintedText = UIColor.tintedText
-    static let action = UIColor.action
     static let paper = UIColor.paper
     static let background = UIColor.background
-    static let navigationBar = UIColor.navigationBar
-    static let toolbar = UIColor.toolBar
-    static let today = UIColor.today
     
     static var dynamicColor: UIColor {
         get {
--- Tag Day/Extension/UIViewController+Extension.swift
+++ Tag Day/Extension/UIViewController+Extension.swift
@@ -9,20 +9,6 @@ import UIKit
 import SafariServices
 
 extension UIViewController {
-    func updateNavigationBarStyle(hideShadow: Bool = false) {
-        let navBarAppearance = UINavigationBarAppearance()
-        navBarAppearance.configureWithOpaqueBackground()
-        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        navBarAppearance.backgroundColor = AppColor.navigationBar
-        if hideShadow {
-            navBarAppearance.shadowColor = UIColor.clear            
-        }
-        navigationController?.navigationBar.standardAppearance = navBarAppearance
-        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
-        navigationController?.navigationBar.tintColor = UIColor.white
-    }
-
     func openSF(with url: URL) {
         let safariViewController = SFSafariViewController(url: url)
         navigationController?.present(safariViewController, animated: ConsideringUser.animated)
