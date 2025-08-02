d847f25d168e1383811391ec888c436e4610e477
zizicici <1940699+zizicici@users.noreply.github.com>
2025-08-02 15:43:40 +0800

    [chore] Bump Version: 1.0.2 -> 1.0.3

--- Tag Day/More/SpecificationsViewController.swift
+++ Tag Day/More/SpecificationsViewController.swift
@@ -73,7 +73,7 @@ class SpecificationsViewController: UIViewController {
                 case .publisher:
                     return "ZIZICICI LIMITED"
                 case .date:
-                    return "2025/07/30"
+                    return "2025/08/02"
                 case .license:
                     return "粤ICP备2025448771号-1A"
                 }
